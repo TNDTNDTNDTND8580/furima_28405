e9731e600f3a9306c913579703e8719c7383d054
TNDTNDTNDTND8580 <68476110+TNDTNDTNDTND8580@users.noreply.github.com>
2020-08-28 12:32:48 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
-  has_one_attached :photo
-  #空の投稿を保存できないようにする
-  # validates :presence: true
+  has_one_attached :image
   belongs_to :user
 
+  validates :image,presence: true
+  validates :item, presence: true
+  validates :introduce, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :area_id,presence: true
+  validates :days_until_shipping_id,presence: true
+  validates :price,numericality: { only_integer: true,greater_than: 300, less_than: 9999999}
 end
+
+# validate :image_presence
+
+# def image_presence
+  
+# end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:image) { FactoryBot.create(:image) }
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('test_image.png')
+  end
+  describe '商品出品情報' do
+    context 'cant save' do
+      it '画像は1枚必須であること' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が必須であること' do
+        @item.item = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item can't be blank")    
+      end
+      it '商品の説明が必須であること' do
+        @item.introduce = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Introduce can't be blank")
+      end
+      it 'カテゴリーの情報が必須であること' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態についての情報が必須であること' do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担についての情報が必須であること' do
+        @item.delivery_fee_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
+      it '発送元の地域についての情報が必須であること' do
+        @item.area_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it '発送までの日数についての情報が必須であること' do
+        @item.days_until_shipping_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days until shipping can't be blank")
+      end
+      it '価格についての情報が必須であること' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
+

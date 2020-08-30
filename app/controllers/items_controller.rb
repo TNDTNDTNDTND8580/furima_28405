1ed448fe1459128fc14f130142d64d2305a0b254
TNDTNDTNDTND8580 <68476110+TNDTNDTNDTND8580@users.noreply.github.com>
2020-08-30 19:29:14 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show, :edit, :destroy, :pay]
   before_action :authenticate_user!, only: [:edit, :destroy, :new, :create, :pay]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def login_check
     unless user_signed_in?
       flash[:alert] = "ログインしてください"
@@ -40,6 +40,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def pay

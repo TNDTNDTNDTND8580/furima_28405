486f036b80c0b0991a9183e456381f31b86035c1
TNDTNDTNDTND8580 <68476110+TNDTNDTNDTND8580@users.noreply.github.com>
2020-08-30 18:30:04 +0900

    商品情報編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :edit, :show, :destroy, :pay] do
+  resources :items, only: [:index, :new, :create, :edit, :update, :show, :destroy, :pay] do
     resources :transactions, only: [:index]
   end
   resources :users, only: [:create, :new]

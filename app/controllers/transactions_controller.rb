3f0abb9cff9f43a3ad20824de2d1b4d4c6f805a8
TNDTNDTNDTND8580 <68476110+TNDTNDTNDTND8580@users.noreply.github.com>
2020-08-31 12:07:06 +0900

    出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,13 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :correct_user, only: :index
   def index
     @items = Item.includes(:user).order("created_at DESC")
+  end
+  def correct_user
+    @items = current_user.items.find_by(id: params[:id])
+      unless @items
+        redirect_to root_url
+      end
   end
 end

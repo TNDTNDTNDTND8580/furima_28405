ea91a6ef0d7f5bc3a09bc3c58df80a6d48faa7d8
TNDTNDTNDTND8580 <68476110+TNDTNDTNDTND8580@users.noreply.github.com>
2020-08-31 11:38:45 +0900

    ログインしていないユーザーは購入ページに遷移しようとすると、ログインページに遷移すること

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,5 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
